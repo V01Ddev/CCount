20055ff1ce8b0e8b9919080edfe122637ecaca52
v01d.dev <x.v.0.1.d.xhacking@gmail.com>
2025-02-13 16:47:33 +0300

    FINALLY, working countdown, yet to add keyboard functions

--- ccount.c
+++ ccount.c
@@ -1,60 +1,79 @@
+#include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <ncurses.h>
-#include "BigMoney-nw.c"
-
-/*
-Rewrite ascii_stycpy to manually write to output array.
-An ascii_strcpy function shouldn't be used, instead an ascii_insert function can insert characters where needed.
-*/
 
 
-void ascii_strcpy(char out[][display_length], char ascii_in[][art_length]){
-    for (int i=0; i<=art_height; i++){
-        for (int j=0; j<art_length; j++){ // < and not <= to exclude the return char
-            out[i][j] = ascii_in[i][j];
-        }
-    }
+void convertSeconds(int total_seconds, int *hours, int *minutes, int *seconds) {
+    *hours = total_seconds / 3600;         // Get the number of hours
+    *minutes = (total_seconds % 3600) / 60; // Get the remaining minutes
+    *seconds = total_seconds % 60;          // Get the remaining seconds
 }
 
+void countdown(int seconds) {
+    initscr();
+    noecho();
+    curs_set(FALSE);
 
-void print_time(WINDOW* win, char ascii_time[][display_length]){
+    int h, m, s;
 
-    int width = art_length;
-    int rows = art_height;
+    for (int i = seconds; i >= 0; i--) {
+        clear();
+        convertSeconds(i, &h, &m, &s);
 
-    // Size of the screen
-    // int max_y = getmaxy(win);
-    // int max_x = getmaxx(win);
-    int max_y = 100;
-    int max_x = 100;
-    
-    // Adjusting for string to be centered.
-    int mid_y = max_y / 2;
-    int mid_x = max_x / 2;
-    int y = mid_y - width / 2;
-    int x = mid_x - rows / 2;
-
-    for (int i=0; i < art_height; i++){
-        mvprintw(y, x, "%s", ascii_time[i]);
-        // printf("%s\n", ascii_time[i]);
-        y++;
+        mvprintw(LINES / 2, (COLS / 2) - 2, "%02d:%02d:%02d", h, m, s);
+        refresh();
+        sleep(1);
     }
-}
 
-int main() {
-
-    WINDOW* win = initscr();
+    mvprintw(LINES / 2 + 1, (COLS / 2) - 6, "Time's up!");
     refresh();
+    sleep(2);
 
-    char display[art_height][display_length];
-
-    ascii_strcpy(display, one);
-    print_time(win, display);
-
-    ascii_strcpy(display, two);
-    print_time(win, display);
-
-    getch(); // Keeps window alive.
     endwin();
+}
+
+int main(int argc, char *argv[]) {
+    // Processing CLI input
+    int h_int = -1;
+    int m_int = -1;
+    int s_int = -1;
+
+    if (argc == 1){
+        printf("No input given!\n");
+        printf("Example inputs:\n");
+        printf("1 30 4\n");
+        printf("0 69m 0\n");
+        printf("5 8\n");
+    }
+    if (argc == 2){
+        s_int = atoi(argv[1]);
+    }
+    if (argc == 3){
+        s_int = atoi(argv[2]);
+        m_int = atoi(argv[1]);
+    }
+    if (argc == 4){
+        s_int = atoi(argv[3]);
+        m_int = atoi(argv[2]);
+        h_int = atoi(argv[1]);
+    }
+    if (argc > 4){
+        printf("Too many arguments given!\n");
+        return 1;
+    }
+
+
+    int seconds=s_int;
+
+    if (h_int!=-1){
+        seconds=seconds+(h_int*60*60);
+    }
+
+    if (m_int!=-1){
+        seconds=seconds+(m_int*60);
+    }
+
+    countdown(seconds);
     return 0;
 }
