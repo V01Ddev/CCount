7785ac502f71bb99020db92889d226b79a848110
v01d.dev <x.v.0.1.d.xhacking@gmail.com>
2025-02-16 13:25:35 +0300

    Added pause functionality and help menu

--- ccount.c
+++ ccount.c
@@ -10,28 +10,55 @@ void convertSeconds(int total_seconds, int *hours, int *minutes, int *seconds) {
     *seconds = total_seconds % 60;          // Get the remaining seconds
 }
 
+
+void paused(){
+    int p = 0;
+    do {
+        if (getch()==32){
+            p = 1;
+        }
+    } while (p==0);
+}
+
+void helpMenu(){
+    printf("[h] Pass your input as arguments\n");
+    printf("[h] Example inputs:\n");
+    printf("[h]     1 30 4  = 1 hour, 30 minutes, and 4 seconds\n");
+    printf("[h]     0 69 0  = 69 minutes\n");
+    printf("[h]     5 8     = 5 minutes and 8 seconds\n");
+    printf("[h] Use Ctrl-C to exit\n");
+    printf("[h] Use -h to get this page\n");
+}
+
+
 void countdown(int seconds) {
     initscr();
     noecho();
     curs_set(FALSE);
+    timeout(0);
+
+    int seconds_left = seconds;
 
     int h, m, s;
 
     for (int i = seconds; i >= 0; i--) {
+        int k = getch();
+        if (k==32){
+            clear();
+            mvprintw(LINES / 2, (COLS / 2) - 2, "Paused");
+            paused();
+        }
+
         clear();
         convertSeconds(i, &h, &m, &s);
-
         mvprintw(LINES / 2, (COLS / 2) - 2, "%02d:%02d:%02d", h, m, s);
         refresh();
         sleep(1);
     }
 
-    mvprintw(LINES / 2 + 1, (COLS / 2) - 6, "Time's up!");
-    refresh();
-    sleep(2);
-
     endwin();
 }
+
 
 int main(int argc, char *argv[]) {
     // Processing CLI input
@@ -40,29 +67,35 @@ int main(int argc, char *argv[]) {
     int s_int = -1;
 
     if (argc == 1){
-        printf("No input given!\n");
-        printf("Example inputs:\n");
-        printf("1 30 4\n");
-        printf("0 69m 0\n");
-        printf("5 8\n");
+        printf("[E] No input given!\n");
+        printf("[h] Use -h to get some help!\n");
+        return 1;
     }
+
     if (argc == 2){
+        if (argv[1][1]=='h'){
+            helpMenu();
+            return 0;
+        }
         s_int = atoi(argv[1]);
     }
+
     if (argc == 3){
         s_int = atoi(argv[2]);
         m_int = atoi(argv[1]);
     }
+
     if (argc == 4){
         s_int = atoi(argv[3]);
         m_int = atoi(argv[2]);
         h_int = atoi(argv[1]);
     }
+
     if (argc > 4){
-        printf("Too many arguments given!\n");
+        printf("[E] Too many arguments given!\n");
+        printf("[h] Use -h to get some help!\n");
         return 1;
     }
-
 
     int seconds=s_int;
 
